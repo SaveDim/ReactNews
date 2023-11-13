751c051d9b4c894f9d08d92d15eb9160dee04d8c
savedim <savedimdev@gmail.com>
2023-11-13 15:01:58 +0300

    Add styles, main page, components

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import {StyleSheet} from "react-native";
+import {gStyle} from "./styles/style";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import Main from "./components/Main";
+// import Contacts from "./components/Contacts";
+
+    const fonts = async () => {
+        await Font.loadAsync({
+            'mt-bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+            'mt-light': require('./assets/fonts/Montserrat-Light.ttf')
+        });
+    };
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    if (fontLoaded) {
+        return (
+            <Main />
+        );
+    } else {
+        return (
+            <AppLoading
+                startAsync={fonts}
+                onFinish={() => setFontLoaded(true)}
+                onError={console.warn}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'silver',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
---
+++ components/Contacts.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {StyleSheet, Text, View} from "react-native";
+import { gStyle } from "../styles/style";
+
+
+export default function Contacts() {
+        return (
+            <View style={gStyle.main}>
+                <Text style={gStyle.title}>About us</Text>
+            </View>
+        );
+}
+
+const styles = StyleSheet.create({
+
+});
